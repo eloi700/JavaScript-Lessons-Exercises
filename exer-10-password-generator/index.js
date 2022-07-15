32c94976f07d278a3abb0680901e66a7f833e42b
Cielo <macielo.tablate@gmail.com>
2022-07-15 22:04:42 +0200

    add exer10

--- exer-10-password-generator/index.js
+++ exer-10-password-generator/index.js
@@ -6,6 +6,14 @@ const numbersEl = document.getElementById('numbers');
 const lengthEl = document.getElementById('length');
 const clipboardEl = document.getElementById('clipboard');
 const generateEl = document.getElementById('generate');
+const resultEl = document.getElementById('result');
+
+const randomFunction = {
+    upper: getRandomUpper,
+    lower: getRandomLower,
+    symbols: getRandomSymbols,
+    numbers: getRandomNumbers
+}
 
 // Generate Functions
 function getRandomUpper(){
@@ -16,16 +24,79 @@ function getRandomLower(){
     return String.fromCharCode(Math.floor(Math.random()*26) + 97);
 }
 
-function getRandomSymbols(){
-    return String.fromCharCode(Math.floor(Math.random()*15) + 33);
-}
-
 // function getRandomSymbols(){
-//     const symbols = "!@#$%^&*(){}[]=<>/,.";
-//     return symbols[Math.floor(Math.random()*symbols.length)];
+//     return String.fromCharCode(Math.floor(Math.random()*15) + 33);
 // }
 
-function getRandomNumbers(){
-    return String.fromCharCode(Math.floor(Math.random)*10 + 48);
+function getRandomSymbols(){
+    const symbols = "!@#$%^&*(){}[]=<>/,.";
+    return symbols[Math.floor(Math.random()*symbols.length)];
 }
+
+function getRandomNumbers(){
+    const numbers = "0123456789";
+    return numbers[Math.floor(Math.random()*numbers.length)];
+    // return String.fromCharCode(Math.floor(Math.random)*10 + 48);
+}
+
+// Generate Event Listener
+generateEl.addEventListener('click', () =>{
+    const hasUpper = uppercaseEl.checked;
+    const hasLower = lowercaseEl.checked;
+    const hasNumber = numbersEl.checked;
+    const hasSymbol = symbolsEl.checked;
+    const length = +lengthEl.value;
+
+    resultEl.innerText = generatePassword(hasLower, hasNumber, hasSymbol, hasUpper, length);
+});
+
+// Generate Password Function
+function generatePassword(lower, numbers, symbols, upper, length){
+    // Initialize password variable
+    let generatedPassword = '';
+
+    // Count the number of the checked values
+    const typesCount = lower + numbers + symbols + upper;
+
+    // Filter out unchecked types - wrapping in curly bracket (setting it as a key) Filter - higher order array method ().
+    // Getting the value wether it false [0] or true
+    const typesArr = [{lower}, {upper}, {numbers}, {symbols}].filter(item =>Object.values(item)[0]);
+
+    // If doesn't have selected type
+    if(typesCount === 0){
+        return "";
+    }
+
+    // Loop over length call generator function for each type
+    // functionName - these are the Object keys such as lower, upper, symbol, number
+    for(let i = 0; i<length; i+=typesCount){
+        typesArr.forEach(type =>{
+            const functionName = Object.keys(type)[0];
+
+            //Add final password to the password variable and return
+            generatedPassword += randomFunction[functionName]();
+        })
+    }
+    const finalPassword = generatedPassword.slice(0, length);
+    return finalPassword;
+}
+
+// Copy Password to Clipboard
+clipboardEl.addEventListener('click', () =>{
+    const textArea = document.createElement('textarea');
+    const password = resultEl.innerText;
+
+    if(!password){
+        return;
+    }
+
+    textArea.value = password;
+    document.body.appendChild(textArea);
+    textArea.select();
+    document.execCommand('copy');
+    textArea.remove();
+    // alert("Password copied to Clipboard");
+})
+
+
 
