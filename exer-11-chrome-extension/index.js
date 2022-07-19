830e26c4c936865434a2cc31af3660bb42897ef1
Cielo <macielo.tablate@gmail.com>
2022-07-19 14:37:59 +0200

    refactor

--- exer-11-chrome-extension/index.js
+++ exer-11-chrome-extension/index.js
@@ -2,43 +2,57 @@ const saveBtn = document.getElementById("save-btn");
 const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-el");
 const deleteBtn = document.getElementById("delete-btn");
+const tabBtn = document.getElementById("tab-btn");
 
 // Store Array in Local Storage
 let myLeads = [];
 let oldLeads = [];
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
-if (leadsFromLocalStorage){
-  myLeads = leadsFromLocalStorage
-  render(myLeads)
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  render(myLeads);
 }
 
+tabBtn.addEventListener("click", () => {
+  chrome.tabs.query(
+    {
+      // tabs key, object query method
+      active: true,
+      currentWindow: true,
+    },
+    (tabs) => {
+      myLeads.push(tabs[0].url);
+      localStorage.setItem("myLeads", JSON.stringify(myLeads));
+      render(myLeads);
+    }
+  );
+});
 // Render the Leads
 function render(leads) {
-  let listItems = "";  // create a list items
-  for (let i = 0; i < leads.length; i++) { //add the link in new tab
+  let listItems = ""; // create a list items
+  for (let i = 0; i < leads.length; i++) {
+    //add the link in new tab
     listItems += `<a href="#" target="_blank"><li> ${leads[i]}</li></a>`;
   }
   ulEl.innerHTML = listItems;
 }
 
-deleteBtn.addEventListener('dblclick', () => {
+deleteBtn.addEventListener("dblclick", () => {
   localStorage.clear();
   myLeads = [];
-  // clearing the DOM by calling renderLeades() function
-  render(myLeads)
+  // clearing the DOM by calling render() function
+  render(myLeads);
 });
 
 saveBtn.addEventListener("click", () => {
-    myLeads.push(inputEl.value); //input value push it into myLeads Array
-    inputEl.value = "";  //clears out the Input Field
+  myLeads.push(inputEl.value); //input value push it into myLeads Array
+  inputEl.value = ""; //clears out the Input Field
 
-    // Saving to Local Storages
-    localStorage.setItem("myLeads", JSON.stringify(myLeads));
-    render(myLeads);
-  });
-
-
+  // Saving to Local Storages
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  render(myLeads);
+});
 
 // TRUTHY OR FALSY
 // falsy Value
@@ -53,5 +67,3 @@ saveBtn.addEventListener("click", () => {
 // console.log(Boolen("100")); - true
 // console.log(Boolen([0])); - true
 // console.log(Boolen(-0)); - false
-
-
