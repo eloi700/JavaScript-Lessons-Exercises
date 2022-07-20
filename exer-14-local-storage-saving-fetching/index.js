ddc72b39d1bc34672bd1901c6e5828d845672c4c
Cielo <macielo.tablate@gmail.com>
2022-07-20 13:42:24 +0200

    add exer14

---
+++ exer-14-local-storage-saving-fetching/index.js
@@ -0,0 +1,9 @@
+//SAVING TO LOCAL STORAGE
+// 1.---Save a value to localStorage
+
+localStorage.setItem("name", "cielo");
+
+// 2.---Delete your code and refresh the page
+// 3.---Fetch your value from localStorage and log it out.
+
+const myName = localStorage.getItem("name");
