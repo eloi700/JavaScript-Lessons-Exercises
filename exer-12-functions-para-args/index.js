e282cd359d794ac3eb7174cb01c9aad962455655
Cielo <macielo.tablate@gmail.com>
2022-07-21 10:08:43 +0200

    fix

--- exer-12-functions-para-args/index.js
+++ exer-12-functions-para-args/index.js
@@ -22,3 +22,31 @@ function getFirst(someArr){
     return someArr[0];
 }
 console.log(getFirst(myArr))
+
+//functions and methods
+//ex. functions prompt(), alert() -> can stand alone when you call it
+//ex. methods -> it is inside of the class -> name.pop()
+
+function sayHello(firstName, lastName){
+    const message = `Hello, ${firstName} ${lastName}`;
+    return message;
+}
+// sayHello();
+
+console.log(sayHello("Eloi", "Tennert"));
+
+//SQUARE A NUMBER
+function square(x){
+    const result = Math.floor(Math.sqrt(x));
+    const desc = `The square root of ${x} is : ${result}`
+    return desc;
+}
+
+console.log(square(9));
+
+//GET RANDOM INTEGER FUNCTION
+function getRandomInt(max){
+    return Math.floor(Math.random() * max);
+}
+
+console.log(getRandomInt(10)) // -> Result from 0 to 10;
