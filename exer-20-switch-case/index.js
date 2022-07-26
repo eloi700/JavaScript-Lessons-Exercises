ec8d2d91e8930009196b628c4a3b447e00d54948
Cielo <macielo.tablate@gmail.com>
2022-07-26 14:57:58 +0200

    edit

--- exer-20-switch-case/index.js
+++ exer-20-switch-case/index.js
@@ -11,6 +11,7 @@ weather.addEventListener("keypress", (e) => {
   }
 });
 
+//SWITCH CASE
 function selectWeatherType() {
   const weatherType = document.getElementById("weather").value;
   switch (weatherType) {
@@ -35,3 +36,32 @@ function selectWeatherType() {
 }
 
 selectWeatherType();
+
+//SWITCH FOR DAY
+
+let day;
+switch (new Date().getDay()) {
+  case 0:
+    day = "Sunday";
+    break;
+  case 1:
+    day = "Monday";
+    break;
+  case 2:
+    day = "Tuesday";
+    break;
+  case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+  case 6:
+    day = "Saturday";
+    break;
+}
+
+console.log(`Today is ${day}.`)
