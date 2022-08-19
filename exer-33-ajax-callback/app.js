2a0fa8c94182d2da59bfde382e343ed10e134490
Cielo <macielo.tablate@gmail.com>
2022-08-19 15:23:30 +0200

    get push put delete http request

--- exer-33-ajax-callback/app.js
+++ exer-33-ajax-callback/app.js
@@ -1,7 +1,32 @@
 //Instantiate
 const http = new easyHTTP();
 
-//GET POSTS
+//GET POSTS as a SOURCE
 http.get("http://jsonplaceholder.typicode.com/posts", (err, posts) => {
   err ? console.log(err) : console.log(posts);
+});
+
+//GET POST (Single)
+http.get("http://jsonplaceholder.typicode.com/posts/1", (err, post) => {
+  err ? console.log(err) : console.log(post);
+});
+
+//CREATE DATA
+const data = {
+    title: "custom post",
+    body: "custom post"
+};
+//CREATE POSTS - ID 101
+http.post("http://jsonplaceholder.typicode.com/posts", data, (err, posts) => {
+  err ? console.log(err) : console.log(posts);
+});
+
+//UPDATE POST/S (Updates whichever ID provided say 5)
+http.put("http://jsonplaceholder.typicode.com/posts/5", data, (err, post) => {
+  err ? console.log(err) : console.log(post);
+});
+
+//DELETE POSTS
+http.delete("http://jsonplaceholder.typicode.com/posts/1", (err, response) => {
+  err ? console.log(err) : console.log(response);
 });
--- exer-33-ajax-callback/easyhttp.js
+++ exer-33-ajax-callback/easyhttp.js
@@ -3,24 +3,58 @@ class easyHTTP {
     this.http = new XMLHttpRequest();
   }
   // Make an HTTP Get Request
-  //Arrow function it works this in ONLOAD however, another option would setting it into a variable called self (lexical this)
-  // let self = this;
+  //Arrow function it works this in ONLOAD however, another option would setting     it into a variable called self (lexical this)...let self = this;
+
   get(url, callback) {
     this.http.open("GET", url, true);
 
     this.http.onload = () => {
       if (this.http.status === 200) {
         callback(null, this.http.responseText);
-      }else{
-        callback('Error:' + this.http.status)
+      } else {
+        callback("Error:" + this.http.status);
+      }
+    };
+
+    this.http.send();
+  }
+
+  // Make an HTTP Post Request
+    post(url, data, callback) {
+      this.http.open("POST", url, true);
+      this.http.setRequestHeader("Content-type", "application/json");
+
+      this.http.onload = () => {
+        callback(null, this.http.responseText);
+      };
+
+      this.http.send(JSON.stringify(data));
+    }
+
+  // Make an HTTP Put Request / Updating the Data
+  put(url, data, callback) {
+    this.http.open("PUT", url, true);
+    this.http.setRequestHeader("Content-type", "application/json");
+
+    this.http.onload = () => {
+      callback(null, this.http.responseText);
+    };
+
+    this.http.send(JSON.stringify(data));
+  }
+
+// Make an HTTP Delete Request
+delete(url, callback){
+    this.http.open("DELETE", url, true);
+
+    this.http.onload = () => {
+      if (this.http.status === 200) {
+        callback(null, 'Post Deleted'); // just an object
+      } else {
+        callback("Error:" + this.http.status);
       }
     };
 
     this.http.send();
   }
 }
-// Make an HTTP Post Request
-
-// Make an HTTP Put Request
-
-// Make an HTTP Delete Request
