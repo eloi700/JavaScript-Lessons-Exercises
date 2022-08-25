c4af71ee5fc1d2edb4c33f9a9394fe817db5e1a5
Cielo <macielo.tablate@gmail.com>
2022-08-25 20:18:43 +0200

    add fetch ext api

--- exer-35-fetch-api/app.js
+++ exer-35-fetch-api/app.js
@@ -1,22 +1,69 @@
-document.getElementById('button1').addEventListener('click', getText);
-document.getElementById('button2').addEventListener('click', getJson);
+document.getElementById("button1").addEventListener("click", getText);
+document.getElementById("button2").addEventListener("click", getJson);
+document.getElementById("button3").addEventListener("click", getExternal);
 
-//----------GET TEXT
+//----------GET TEXT DATA
 //fetch returns Promises(.then)
-function getText(){
-    fetch('test.txt')
-    .then((res) => { // catch the promise
-        return res.text() // res.json()
+function getText() {
+  fetch("test.txt")
+    .then((res) => {
+      // catch the promise
+      return res.text(); // res.json()
     })
     //in data is that what promise returns
-    .then((data) =>{ // grab the data
-        console.log(data);
-        document.getElementById('output').innerHTML = data;
+    .then((data) => {
+      // grab the data
+      console.log(data);
+      document.getElementById("output").innerHTML = data;
     })
     //if there's error
-    .catch((err) =>{
-        console.log(err);
-    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
 
-//---------GET JSON
+//---------GET LOCAL JSON DATA
+function getJson() {
+  fetch("posts.json")
+    .then((res) => {
+      // catch the promise
+      return res.json();
+    })
+    //in data is that what promise returns
+    .then((data) => {
+      // grab the data
+      console.log(data);
+      let output = "";
+      data.forEach((post) => {
+        output += `<li>${post.title}</li>`;
+      });
+      document.getElementById("output").innerHTML = output;
+    })
+    //if there's error
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+//---------GET FROM EXTERNAL API
+function getExternal() {
+  fetch("https://api.github.com/users")
+    .then((res) => {
+      // catch the promise
+      return res.json();
+    })
+    //in data is that what promise returns
+    .then((data) => {
+      // grab the data
+      console.log(data);
+      let output = "";
+      data.forEach((user) => {
+        output += `<li>${user.login}</li> <span> ${user.id} </span>`;
+      });
+      document.getElementById("output").innerHTML = output;
+    })
+    //if there's error
+    .catch((err) => {
+      console.log(err);
+    });
+}
